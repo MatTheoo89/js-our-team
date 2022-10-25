52c1e21f7d41866dca1c66c80a078cacc84fe196
MatTheoo89 <matteobellomo89@gmail.com>
2022-10-25 15:44:44 +0200

    [add] MILESTONE 3

--- assets/js/app.js
+++ assets/js/app.js
@@ -34,20 +34,44 @@ const staff = [
 ];
 
 // MILESTONE 0 Mi restituisce in console ogni singolo onj come array
-console.log(staff);
+//console.log(staff);
 
-// MILESTONE 1 Stampare su console le informazioni di nome, ruolo e la stringa della foto
+// // MILESTONE 1 Stampare su console le informazioni di nome, ruolo e la stringa della foto
+
+// for(let individual of staff){
+//   const output = `${individual.name} - ${individual.role} - assets/img/${individual.image} `
+//   console.log(output);
+// }
+
+// // MILESTONE 2 Stampare le stesse informazioni su DOM sotto forma di stringhe
+// const lista = document.querySelector('.lista')
+// for(let individual of staff){
+//   const output = `<li>${individual.name} - ${individual.role} - assets/img/${individual.image} </li>`
+//   lista.innerHTML += output
+// }
+
+// MILESTONE 3 Stampare delle card formattate contenete immagini e testo
+const row = document.querySelector(".row");
 
 for(let individual of staff){
-  const output = `${individual.name} - ${individual.role} - assets/img/${individual.image} `
-  console.log(output);
+  //console.log(individual);
+
+  let card = `
+  
+  <div class="col-4">
+  
+    <div class="card" style="width: 18rem;">
+      <div class="card-image">
+        <img src="assets/img/${individual.image}" class="card-img-top" alt="${individual.name}">
+      </div>
+      <div class="card-body">
+        <p class="card-text text-center">${individual.name}</p>
+        <p class="card-text text-center">${individual.role}</p>
+      </div>
+    </div>
+  
+  </div>
+  `
+  row.innerHTML += card;
 }
 
-// MILESTONE 2 Stampare le stesse informazioni su DOM sotto forma di stringhe
-const lista = document.querySelector('.lista')
-for(let individual of staff){
-  const output = `<li>${individual.name} - ${individual.role} - assets/img/${individual.image} </li>`
-  lista.innerHTML += output
-}
-
-// MILESTONE 3 Stampare delle card formattate contenete immagini e testo (La grafica fornita è solo un esempio ma sentitevi liberi di “graficare” come preferite)
